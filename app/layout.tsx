de4428ee1da69cc0389cd914a5dd8bdb57a34741
ddoniddoni <psdkei@naver.com>
2026-01-04 00:51:51 +0900

    design(ui): header, footer 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -18,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${outfit.className} antialiased`}>
-        <header>mymy</header>
+        <Header />
         {children}
-        <footer>mymy Inc. All rights reserved.</footer>
+        <Footer />
       </body>
     </html>
   );
--- components/landing-page/recently-launched-products.tsx
+++ components/landing-page/recently-launched-products.tsx
@@ -1,15 +1,58 @@
-import { RocketIcon } from "lucide-react";
+import { CalendarIcon, RocketIcon } from "lucide-react";
 import SectionHeader from "../common/section-header";
+import FeaturedProducts from "./featured-products";
+import ProductCard from "../products/product-card";
+import EmptyState from "../common/empty-state";
 
 export default function RecentlyLaunchedProducts() {
+  const recentlyLaunchedProducts = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "Description 1",
+      tags: ["Saas", "Pricing", "Global"],
+      votes: 615,
+      isFeatured: true,
+    },
+    {
+      id: 2,
+      name: "Product 1",
+      description: "Description 1",
+      tags: ["Saas", "Pricing", "Global"],
+      votes: 615,
+      isFeatured: true,
+    },
+    {
+      id: 3,
+      name: "Product 1",
+      description: "Description 1",
+      tags: ["Saas", "Pricing", "Global"],
+      votes: 615,
+      isFeatured: true,
+    },
+  ];
+
   return (
     <section className="py-20">
-      <div className="wrapper">
+      <div className="wrapper space-y-12">
         <SectionHeader
-          title="최근 출시된 제품"
+          title="Recently Launched"
           icon={RocketIcon}
           description="Discover the latest products from our community"
         />
+
+        {recentlyLaunchedProducts.length > 0 ? (
+          <div className="grid-wrapper">
+            {recentlyLaunchedProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        ) : (
+          <EmptyState
+            message="No products launched in the last week. Check back soon for new launches."
+            icon={CalendarIcon}
+          />
+        )}
       </div>
     </section>
   );
